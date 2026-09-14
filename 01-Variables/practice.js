6db00ac2cdb364001ef31645d86247944df95caa
Farzana Akter <farzanaakteromi93@gmail.com>
2026-09-14 23:44:26 +0600

    practice.js

---
+++ 01-Variables/practice.js
@@ -0,0 +1,48 @@
+// JavaScript Variables Practice
+
+// 1. Using let
+let name = "Farzana";
+let age = 20;
+
+console.log(name);
+console.log(age);
+
+// 2. Changing a let variable
+let score = 50;
+
+console.log(score);
+
+score = 90;
+
+console.log(score);
+
+// 3. Using const
+const university = "Southeast University";
+
+console.log(university);
+
+// 4. Multiple variables
+let city = "Dhaka";
+let department = "CSE";
+let semester = 9;
+
+console.log(city);
+console.log(department);
+console.log(semester);
+
+// 5. Practice
+// Create your own variables for:
+// - Your name
+// - Your age
+// - Your university
+// - Your CGPA
+
+let myName = "Your Name";
+let myAge = 20;
+const myUniversity = "Your University";
+let cgpa = 3.75;
+
+console.log(myName);
+console.log(myAge);
+console.log(myUniversity);
+console.log(cgpa);
