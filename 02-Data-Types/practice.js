3fdc9f8439ed2afa9b0f50f0bc091447191f074c
Farzana Akter <farzanaakteromi93@gmail.com>
2026-09-14 23:51:29 +0600

    practice.js

---
+++ 02-Data-Types/practice.js
@@ -0,0 +1,57 @@
+// JavaScript Data Types Practice
+
+// 1. String
+let name = "Farzana";
+
+console.log(name);
+console.log(typeof name);
+
+// 2. Number
+let age = 20;
+let cgpa = 3.75;
+
+console.log(age);
+console.log(typeof age);
+
+console.log(cgpa);
+console.log(typeof cgpa);
+
+// 3. Boolean
+let isStudent = true;
+
+console.log(isStudent);
+console.log(typeof isStudent);
+
+// 4. Undefined
+let result;
+
+console.log(result);
+console.log(typeof result);
+
+// 5. Null
+let selectedUser = null;
+
+console.log(selectedUser);
+console.log(typeof selectedUser);
+
+// 6. Array
+let languages = ["JavaScript", "Python", "C++"];
+
+console.log(languages);
+console.log(typeof languages);
+
+// 7. Object
+let student = {
+    name: "Farzana",
+    age: 20,
+    department: "CSE"
+};
+
+console.log(student);
+console.log(typeof student);
+
+// 8. BigInt
+let bigNumber = 12345678901234567890n;
+
+console.log(bigNumber);
+console.log(typeof bigNumber);
